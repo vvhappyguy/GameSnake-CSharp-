f53e688195afa05684d56627ba72f5cbd55f9d3e
Iowan <dyakin_ip@outlook.com>
2016-06-12 19:10:10 +0300

    First '*' + Console.Write

--- GameSnake/Program.cs
+++ GameSnake/Program.cs
@@ -10,7 +10,12 @@ namespace GameSnake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.WriteLine(sym1);
             Console.ReadLine();
         }
     }
