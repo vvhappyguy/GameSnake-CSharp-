6152ea32d28e9ccd7436fd24de93eb8d3758cabc
Iowan <dyakin_ip@outlook.com>
2016-06-18 23:14:50 +0300

    Deleting non-useful

--- GameSnake/Program.cs
+++ GameSnake/Program.cs
@@ -11,20 +11,9 @@ namespace GameSnake
         static void Main(string[] args)
         {
             point p1 = new point(1, 3, '*');
-            /*p1.x = 1;
-            p1.y = 2;
-            p1.sym = '*';*/
             p1.Draw();
 
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(p1.x, p1.y, p1.sym);*/
-
-            point p2 = new point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            point p2 = new point(4, 5, '#');
             p2.Draw();
 
             Console.ReadLine();
