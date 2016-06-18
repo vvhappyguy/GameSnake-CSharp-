8a49608f88544fcfc587d45a054218c37ce46408
Iowan <dyakin_ip@outlook.com>
2016-06-18 22:28:51 +0300

    First class/object First method Draw();

--- GameSnake/Program.cs
+++ GameSnake/Program.cs
@@ -10,26 +10,24 @@ namespace GameSnake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 2;
+            p1.sym = '*';
+            p1.Draw();
+
+            /*int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
+            Draw(p1.x, p1.y, p1.sym);*/
 
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-
-            Draw(x2, y2, sym2);
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
